36ba27049f20d1b50c9df559fc9e1ef2e2d060c6
BasD <basdoppen@gmail.com>
2022-05-13 13:55:28 +0200

    notes+ timeline

--- _dist/ccc/js/header2Navigation.js
+++ _dist/ccc/js/header2Navigation.js
@@ -47,3 +47,25 @@ function header2Navigation(contentId, navId, asList) {
 
   document.getElementById(navId).innerHTML += preBlock+headerNavigation+postBlock;
 }
+
+
+let positionTimeline = () => {
+  let tlBlock = document.getElementById('timelineBlock').outerHTML
+  document.getElementById('timelineBlock').outerHTML = ''
+
+
+  // get all headers 2 and 3.
+  let contentElem = document.getElementById('ccc_main_content');
+  let elems = contentElem.querySelectorAll('h2');
+
+  // adb anchor link to each header
+  for (var i = 0; i < elems.length; i++) {
+    if (elems[i].innerHTML == 'Timeline') {
+      let div = document.createElement("div");
+      div.innerHTML = tlBlock
+
+      elems[i].after(div);
+    }
+  }
+}
+positionTimeline()
--- src/js/notesPopup.js
+++ src/js/notesPopup.js
@@ -1,8 +1,42 @@
+let popupUP = false;
 
 
-function handleNotes(nr) {
-  console.log(nr);
+let elements = document.getElementsByClassName("footnote-link");
+
+
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', handleNotes, false);
+    elements[i].removeAttribute('href');
 }
-J*PwkH@yjV4xareW
-pAGpz@GT!kz@qUS9
-#)wJ5dNQyQfxC(*4
+
+
+
+
+function handleNotes() {
+  popupState()
+
+  let elemId = this.id
+
+  let noteNr = document.getElementById(elemId).innerHTML
+  let newId = elemId.replace('identifier_', 'footnote_')
+
+  let newContent = document.getElementById(newId).innerHTML
+  newContent = newContent.replace('identifier_', 'footnote_')
+  let noteNrTxt = '<strong>Note '+noteNr+'</strong><br>'
+  let closeButton = '<button onclick="popupState()" id="closeNote">X</button>'
+
+  document.getElementById("notesPopInner").innerHTML = closeButton+noteNrTxt+newContent
+}
+
+
+
+function popupState() {
+
+  if (popupUP) {
+    document.getElementById("notePop").style.bottom = '-400px'
+    popupUP = false
+  }else {
+    document.getElementById("notePop").style.bottom = '0px'
+    popupUP = true
+  }
+}
